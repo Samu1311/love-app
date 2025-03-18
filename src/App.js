768589d8cf2571e03fb532696e7876592098b8ce
Samuele <samuele13112002@gmail.com>
2025-03-18 14:06:57 +0100

    Refactor routing structure to use nested routes with HashRouter and simplify App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import LoveMap from "./pages/LoveMap";
-import Scrapbook from "./pages/Scrapbook";
-import Quiz from "./pages/Quiz";
-import MusicBox from "./pages/MusicBox";
-import PlayingSong from "./pages/PlayingSong";
+import Home from './pages/Home';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/love-map" element={<LoveMap />} />
-        <Route path="/scrapbook" element={<Scrapbook />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/music-box" element={<MusicBox />} />
-        <Route path="/playing-song" element={<PlayingSong />} />
-      </Routes>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Home from "./pages/Home";
+import LoveMap from "./pages/LoveMap";
+import Scrapbook from "./pages/Scrapbook";
+import Quiz from "./pages/Quiz";
+import MusicBox from "./pages/MusicBox";
+import PlayingSong from "./pages/PlayingSong";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="love-map" element={<LoveMap />} />
+          <Route path="scrapbook" element={<Scrapbook />} />
+          <Route path="quiz" element={<Quiz />} />
+          <Route path="music-box" element={<MusicBox />} />
+          <Route path="playing-song" element={<PlayingSong />} />
+        </Route>
+      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
